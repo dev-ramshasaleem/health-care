beb38d21de080e216ebac22c2f28d23b60c8aa58
dev-ramshasaleem <ramshasaleem25@gmail.com>
2026-06-29 18:18:21 +0500

    first commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className=" mt-2 center-justify border border-gray rounded mx-auto bg-white-300 w-200 py-2">
+      navbar
+    </header>
+  );
+};
+
+export default Navbar;
