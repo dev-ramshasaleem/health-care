2e179e1ed26c9bda90ab2bed4b90cdb4eb4ee9f1
dev-ramshasaleem <ramshasaleem25@gmail.com>
2026-07-07 16:13:35 +0500

    main

--- lib/auth.ts
+++ lib/auth.ts
@@ -14,4 +14,10 @@ export const auth = betterAuth({
   secret: process.env.BETTER_AUTH_SECRET,
 
   baseURL: process.env.BETTER_AUTH_URL,
+  trustedOrigins: [
+    "http://localhost:3000",
+    "https://health-care-sand-eight.vercel.app",
+  ],
+
+  
 });
